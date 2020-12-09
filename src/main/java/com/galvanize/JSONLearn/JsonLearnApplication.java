86bc2c48b471137fa3e6d236b21aee193ed929fa
Robert-F-Cameron <robert.f.cameron@gmail.com>
2020-12-09 09:38:08 -0800

    first commit

---
+++ src/main/java/com/galvanize/JSONLearn/JsonLearnApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.JSONLearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonLearnApplication.class, args);
+	}
+
+}
