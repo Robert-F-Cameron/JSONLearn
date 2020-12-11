199ee85447ebf42b85228c8db0fc645d9d10200c
Robert-F-Cameron <robert.f.cameron@gmail.com>
2020-12-11 11:56:12 -0800

    added testing

--- src/test/java/com/galvanize/FlightEndpoints/LessonControllerTest.java
+++ src/test/java/com/galvanize/FlightEndpoints/LessonControllerTest.java
@@ -4,36 +4,60 @@ import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
+import javax.transaction.Transactional;
+import java.beans.Transient;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(LessonController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class LessonControllerTest {
     @Autowired
     private MockMvc mvc;
 
-    @BeforeEach
-    public void postFlightsGetTotal() throws Exception {
-        String json = getJSON("/data.json").toString();
-        this.mvc.perform(
-                post("/flights/tickets/total")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(json))
+    @Autowired
+    LessonRepository repository;
+
+
+    @Test
+    @Transactional
+    public void testCreate() throws Exception{
+        MockHttpServletRequestBuilder request = post("/lessons")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"title\": \"SQL\"}");
+        this.mvc.perform(request)
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.result", is(350)));
+                .andExpect(jsonPath("$.id", instanceOf(Number.class) ))
+                .andExpect(jsonPath("$.title", is("SQL") ));
     }
-    private String getJSON(String path) throws Exception {
-        URL url = this.getClass().getResource(path);
-        return new String(Files.readAllBytes(Paths.get(url.toURI())));
+    @Test
+    @Transactional
+    public void testGetById() throws Exception{
+        MockHttpServletRequestBuilder request = post("/lessons")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"title\": \"SQL\"}");
+        this.mvc.perform(request);
+        this.mvc.perform(
+                get("/lessons/1")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", is(1)));
+
     }
 }
